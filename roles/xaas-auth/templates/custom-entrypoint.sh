75416422f719e9d6691098fba096fc1d89263a2a
Dominique Quatravaux <dominique.quatravaux@epfl.ch>
2023-01-30 17:15:43 +0100

    [fix] Variable (randomly-picked?) `ID=` attribute in backend.yml
    Just `sed` it away ☺

--- roles/xaas-auth/templates/custom-entrypoint.sh
+++ roles/xaas-auth/templates/custom-entrypoint.sh
@@ -9,6 +9,7 @@ docker_pprint_metadata () {
 	# use the SAML2 backend keymat to temporarily sign the generated metadata
 	touch backend.xml frontend.xml
 	satosa-saml-metadata proxy_conf.yaml backend.key backend.crt
+        sed -i 's/ID="id-[^"]*"//' backend.xml
 
 	echo -----BEGIN SAML2 BACKEND METADATA-----
 	xq -x 'del(."ns0:EntityDescriptor"."ns1:Signature")' backend.xml | tee backend.xml.new
