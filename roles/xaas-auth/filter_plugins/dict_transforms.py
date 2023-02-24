2fd8a76266467cdbb513b2d983283183629d1ad1
Dominique Quatravaux <dominique.quatravaux@epfl.ch>
2023-02-24 17:11:52 +0100

    [workaround] limitations in SATOSA's SAML2 and OIDC implementations
    - Must set `allow_unsolicited: true` to avoid a silly `saml2.response.UnsolicitedResponse` stack trace
    - [Apparently](https://github.com/IdentityPython/pyop/blob/3658fdcf6a2467582598cc9247a044ec39cbc37f/src/pyop/client_authentication.py#L62), [pyop](https://github.com/IdentityPython/pyop) presumes that the token retrieval method proceeds using basic auth. As git blame says “Initial import of all code,” I'm not going to waste my time figuring out who is in the wrong here; fact is, OAuth2 authorization code flow most certainly does *not* require basic auth.

--- roles/xaas-auth/filter_plugins/dict_transforms.py
+++ roles/xaas-auth/filter_plugins/dict_transforms.py
@@ -1,10 +1,21 @@
 """Parsing Python is hard, let's have Jinja filters instead."""
 
+from copy import copy
+
 class FilterModule(object):
     def filters(self):
         return {
-            'dictify': self.dictify
+            'dictify': self.dictify,
+            'map_dict_defaults' : self.map_dict_defaults
         }
 
     def dictify(self, l, by_key):
         return dict((item[by_key], item) for item in l)
+
+    def map_dict_defaults(self, l, defaults):
+        return [self.dict_merge(defaults, item) for item in l]
+
+    def dict_merge(self, d1, d2):
+        d = d1.copy()
+        d.update(d2)
+        return d
